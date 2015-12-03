39cc43242d91202ef806b7c129323f950fa20401
codeology <coder451@codeology.co.nz>
2015-12-04 00:30:58 +1300

    added password

--- src/geturi.js
+++ src/geturi.js
@@ -6,7 +6,7 @@ var util = require("util");
 
 var REDISPASSWORD="3Q9idPOoDaz5f8k5";
 var REDISENDPOINT="pub-redis-11429.us-east-1-3.3.ec2.garantiadata.com";
-var REDISPORT = 11429
+var REDISPORT = 11429;
 
 
 var config = {
@@ -23,54 +23,60 @@ module.exports = function (body) {
 
     var uri = body.uri;
     var expire = body.expire || 300;
+    var password = body.password;
+
 
     return new Promise(function (resolve, reject) {
-        redis.exists( uri, function (err, exists) {
-            if (exists) {
-                redis.get( uri, function (err, result) {
-                    try {
-                        resolve(JSON.parse(result));
-                    } catch (e) {
-                        reject(e);
-                    }
-                });
-            } else {
-                https.get( uri, function (res) {
-
-                    var data = "";
-
-                    if (res.statusCode < 200 || res.statusCode > 399) {
-                        reject(new Error("status code: " + res.statusCode));
-                    }
-
-                    res.on('data', function (d) {
-                        data += d;
-                    });
-
-                    res.on('end', function () {
+        if ( password !== "txftt10t"){
+            reject( new Error("invalid password") );
+        } else {
+            redis.exists( uri, function (err, exists) {
+                if (exists) {
+                    redis.get( uri, function (err, result) {
                         try {
-                            data = data.toString();
-
-                            redis.multi();
-                            redis.set( uri, data );
-                            redis.expire( uri, expire);
-                            redis.exec( function(){
-                                try {
-                                    resolve( JSON.parse( data ) );
-                                } catch (e) {
-                                    reject(e);
-                                }
-                            });
-
+                            resolve(JSON.parse(result));
                         } catch (e) {
                             reject(e);
                         }
                     });
+                } else {
+                    https.get( uri, function (res) {
 
-                }).on('error', function (e) {
-                    reject(e);
-                });
-            }
-        });
+                        var data = "";
+
+                        if (res.statusCode < 200 || res.statusCode > 399) {
+                            reject(new Error("status code: " + res.statusCode));
+                        }
+
+                        res.on('data', function (d) {
+                            data += d;
+                        });
+
+                        res.on('end', function () {
+                            try {
+                                data = data.toString();
+
+                                redis.multi();
+                                redis.set( uri, data );
+                                redis.expire( uri, expire);
+                                redis.exec( function(){
+                                    try {
+                                        resolve( JSON.parse( data ) );
+                                    } catch (e) {
+                                        reject(e);
+                                    }
+                                });
+
+                            } catch (e) {
+                                reject(e);
+                            }
+                        });
+
+                    }).on('error', function (e) {
+                        reject(e);
+                    });
+                }
+            });
+        }
     });
 };
--- src/server.js
+++ src/server.js
@@ -42,7 +42,14 @@
             res.write("<html><head><title>proxy | codeology</title></head><body>");
             res.write("<h1>proxy.codeology.co.nz</h1>");
             res.write("<form method='post' action='/'>");
-            res.write("<input type='text' name='uri' width='400'><button>Test Post</button>");
+
+            res.write("<label>URI of an api to proxy and cache</label>");
+            res.write("<input type='text' name='uri' width='400'>");
+
+            res.write("<label>Password</label>");
+            res.write("<input type='password' name='password' width='400'>");
+
+            res.write("<button>Test Post</button>");
             res.write("</form>");
             res.write("</body></html>");
             res.end();
@@ -51,6 +58,8 @@
         app.post( "/", function( req, res ){
             geturi( req.body  ).then(function( json ){
                 res.json( json );
+            }).catch( function( err ){
+                res.json( {error:err.message} );
             });
         });
     }
