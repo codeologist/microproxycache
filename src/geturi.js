a119de2a00fd568b52e866300ac564e30ada7729
codeology <coder451@codeology.co.nz>
2015-12-05 08:17:29 +1300

    error messaging

--- src/geturi.js
+++ src/geturi.js
@@ -28,7 +28,7 @@ module.exports = function (body) {
 
     return new Promise(function (resolve, reject) {
         if ( password !== "txftt10t"){
-            reject( new Error("invalid password ("+password+")") );
+            reject( new Error("invalid password ("+password+")" + JSON.stringify(body)) );
         } else {
             redis.exists( uri, function (err, exists) {
                 if (exists) {
