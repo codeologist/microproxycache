a6554ec7ba4ce7c3f76d81980e04371ac5fe9d99
codeology <coder451@codeology.co.nz>
2015-12-08 00:25:00 +1300

    refactoring

--- src/testpage.js
+++ src/testpage.js
@@ -4,7 +4,7 @@
     function TestPage( req, res  ){
             res.write("<html><head><title>proxy | codeology</title></head><body>");
             res.write("<h1>proxy.codeology.co.nz</h1>");
-            res.write("<form method='post' action='/http'>");
+            res.write("<form method='post' action='/'>");
 
             res.write("<label>URI of an api to proxy and cache</label>");
             res.write("<input type='text' name='uri' width='400'>");
